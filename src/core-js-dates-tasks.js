28897fc4c90f974513ea5414b8ddab8d46117f04
Gasanova Tamara <solas93@mail.ru>
2024-03-22 17:24:18 +0300

    feat: implement getDayName function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -51,8 +51,20 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const months = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const dateArg = new Date(date);
+
+  const getDay = dateArg.getDay();
+  return months[getDay];
 }
 
 /**
